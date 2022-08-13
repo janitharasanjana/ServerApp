a7dbb5a3e071e34768c5903ba218eba3c3d9af20
janitharasanjana <janitharasanjana59@gmail.com>
2022-08-13 20:38:24 +0530

    add print Writer

--- ServerSocketProgramming/Modul1/src/lk/ijse/ClientApp.java
+++ ServerSocketProgramming/Modul1/src/lk/ijse/ClientApp.java
@@ -1,6 +1,7 @@
 package lk.ijse;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.net.InetAddress;
 import java.net.Socket;
 
@@ -9,5 +10,7 @@ public class ClientApp {
         final int PORT = 8001;
         Socket socket = new Socket("localhost",PORT);
         System.out.println();
+        PrintWriter printWriter = new PrintWriter(socket.getOutputStream()); // we can use this some one or thing to print
+        printWriter.println("hello server,kohomada oyata? ");
     }
 }
