4c4b86c99924792644c57f5967526cb825d396df
janitharasanjana <janitharasanjana59@gmail.com>
2022-08-13 23:33:03 +0530

    update module 2

--- ServerSocketProgramming/Module2/src/lk/ijse/ClientApp.java
+++ ServerSocketProgramming/Module2/src/lk/ijse/ClientApp.java
@@ -1,4 +1,16 @@
 package lk.ijse;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.Socket;
+
 public class ClientApp {
+    public static void main(String[] args) throws IOException {
+        final int PORT = 8001;
+        Socket socket = new Socket("localhost",PORT);
+        System.out.println();
+        PrintWriter printWriter = new PrintWriter(socket.getOutputStream()); // we can use this some one or thing to print
+        printWriter.println("hello server,kohomada oyata? ");
+        printWriter.flush();
+    }
 }
--- ServerSocketProgramming/Module2/src/lk/ijse/ServerApp.java
+++ ServerSocketProgramming/Module2/src/lk/ijse/ServerApp.java
@@ -1,4 +1,27 @@
 package lk.ijse;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class ServerApp {
+    public static void main(String[] args) throws IOException {
+        final int PORT = 8001;
+        ServerSocket serverSocket  = new ServerSocket(PORT);
+        System.out.println("Server is running in port " +PORT);
+
+        Socket localSocket = serverSocket.accept();
+
+        System.out.println("remote socket port " + localSocket.getPort());
+        System.out.println("remote socket ip " + localSocket.getInetAddress());
+
+        InputStreamReader inputStreamReader = new InputStreamReader(localSocket.getInputStream());
+
+        BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+
+        String clientMsg = bufferedReader.readLine();
+        System.out.println("client says : " + clientMsg);
+    }
 }
