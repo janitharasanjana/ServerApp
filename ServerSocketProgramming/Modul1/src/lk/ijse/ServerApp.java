25b90e63fcd5e72d41ba1c399be4c89b9f7a9887
janitharasanjana <janitharasanjana59@gmail.com>
2022-08-13 13:23:34 +0530

    initial Contents of Server Socket Programming

---
+++ ServerSocketProgramming/Modul1/src/lk/ijse/ServerApp.java
@@ -0,0 +1,15 @@
+package lk.ijse;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class ServerApp {
+
+    public static void main(String[] args) throws IOException {
+        final int PORT = 8000;
+        ServerSocket serverSocket  = new ServerSocket(PORT);
+        System.out.println("Server is running in port " +PORT);
+        Socket localSocket = serverSocket.accept();
+    }
+}
