041dc6e4d0aee018108926a358cd7473e53f587b
janitharasanjana <janitharasanjana59@gmail.com>
2022-08-13 14:38:39 +0530

    update more codes to client app

--- ServerSocketProgramming/Modul1/src/lk/ijse/ClientApp.java
+++ ServerSocketProgramming/Modul1/src/lk/ijse/ClientApp.java
@@ -1,4 +1,13 @@
 package lk.ijse;
 
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.Socket;
+
 public class ClientApp {
+    public static void main(String[] args) throws IOException {
+        final int PORT = 8001;
+        Socket socket = new Socket("localhost",PORT);
+        System.out.println();
+    }
 }
