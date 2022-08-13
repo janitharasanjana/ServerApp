39148e1bc045aa7b5df698e4ad82fe83925d9772
janitharasanjana <janitharasanjana59@gmail.com>
2022-08-13 13:57:28 +0530

    get remote socket port

--- ServerSocketProgramming/Modul1/src/lk/ijse/ServerApp.java
+++ ServerSocketProgramming/Modul1/src/lk/ijse/ServerApp.java
@@ -7,9 +7,13 @@ import java.net.Socket;
 public class ServerApp {
 
     public static void main(String[] args) throws IOException {
-        final int PORT = 8000;
+        final int PORT = 8001;
         ServerSocket serverSocket  = new ServerSocket(PORT);
         System.out.println("Server is running in port " +PORT);
+
         Socket localSocket = serverSocket.accept();
+
+        System.out.println("remote socket port " + localSocket.getPort());
+        //System.out.println("remote socket id " + localSocket.getInetAddress());
     }
 }
