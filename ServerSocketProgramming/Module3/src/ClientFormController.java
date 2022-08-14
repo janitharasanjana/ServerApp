9bd6edc00026b05e1f83c5fe076674552db53fbc
janitharasanjana <janitharasanjana59@gmail.com>
2022-08-14 13:09:25 +0530

    create scean build forms

--- ServerSocketProgramming/Module3/src/ClientFormController.java
+++ ServerSocketProgramming/Module3/src/ClientFormController.java
@@ -1,2 +1,11 @@
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
 public class ClientFormController {
+    public TextArea txtClientArea;
+    public TextField txtClientMessage;
+
+    public void clientSendOnAction(ActionEvent actionEvent) {
+    }
 }
--- ServerSocketProgramming/Module3/src/ServerFormController.java
+++ ServerSocketProgramming/Module3/src/ServerFormController.java
@@ -1,2 +1,11 @@
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
 public class ServerFormController {
+    public TextArea txtArea;
+    public TextField txtMessage;
+
+    public void sendOnAction(ActionEvent actionEvent) {
+    }
 }
