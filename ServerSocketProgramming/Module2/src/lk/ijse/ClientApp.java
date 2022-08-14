b525d2b895362d4a331f7040a484baddf4293455
janitharasanjana <janitharasanjana59@gmail.com>
2022-08-14 10:51:48 +0530

    update code

--- ServerSocketProgramming/Module2/src/lk/ijse/ClientApp.java
+++ ServerSocketProgramming/Module2/src/lk/ijse/ClientApp.java
@@ -1,16 +1,41 @@
 package lk.ijse;
 
-import java.io.IOException;
-import java.io.PrintWriter;
+import java.io.*;
 import java.net.Socket;
 
 public class ClientApp {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args){
         final int PORT = 8001;
-        Socket socket = new Socket("localhost",PORT);
-        System.out.println();
+        try {
+            Socket socket = new Socket("localhost", PORT);
+
+       /* System.out.println();
         PrintWriter printWriter = new PrintWriter(socket.getOutputStream()); // we can use this some one or thing to print
         printWriter.println("hello server,kohomada oyata? ");
-        printWriter.flush();
+        printWriter.flush(); */
+
+            DataOutputStream dataOutputStream = new DataOutputStream(socket.getOutputStream());  // yawana dewal yawanna
+            DataInputStream dataInputStream = new DataInputStream(socket.getInputStream());  // ena data ganna
+
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+            String message = "" , reply = "" ;
+
+            while (!message.equals("finish")){
+                reply = bufferedReader.readLine();
+                dataOutputStream.writeUTF(reply);
+                message=dataInputStream.readUTF();
+                System.out.println(message);
+
+                dataOutputStream.flush();
+            }
+
+            dataInputStream.close();
+            dataOutputStream.close();
+            bufferedReader.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
