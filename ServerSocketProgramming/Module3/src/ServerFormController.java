781d48270cc3df82522bfe62043267d03ca983e2
janitharasanjana <janitharasanjana59@gmail.com>
2022-08-14 13:34:40 +0530

    convert to lamda

--- ServerSocketProgramming/Module3/src/ServerFormController.java
+++ ServerSocketProgramming/Module3/src/ServerFormController.java
@@ -4,6 +4,7 @@ import javafx.scene.control.TextField;
 
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
+import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -11,18 +12,21 @@ public class ServerFormController {
     private TextArea txtArea;
 
     private TextField txtMessage;
+    final int PORT = 8000;
     ServerSocket serverSocket;
     Socket accept;
     DataInputStream dataInputStream;
     DataOutputStream dataOutputStream;
 
     public void initialize (){
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                
+        new Thread(() -> {
+
+            try {
+                serverSocket = new ServerSocket(PORT);
+            } catch (IOException e) {
+                e.printStackTrace();
             }
-        })
+        }).start();
 
     }
 
